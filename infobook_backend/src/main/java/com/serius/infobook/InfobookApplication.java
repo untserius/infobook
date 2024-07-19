8f6c9d644970e32d49cb5798b174baeafa84f622
untserius <sudhirdas1999@gmail.com>
2024-07-19 12:05:46 +0530

    t

--- infobook_backend/src/main/java/com/serius/infobook/InfobookApplication.java
+++ infobook_backend/src/main/java/com/serius/infobook/InfobookApplication.java
@@ -10,7 +10,6 @@ public class InfobookApplication {
 		SpringApplication.run(InfobookApplication.class, args);
 	}
 
-	// Changes from remote 2
-
+	// Changes from remote 3
 
 }
---
+++ infobook_backend/src/main/java/com/serius/infobook/MyChanges.java
@@ -0,0 +1,5 @@
+package com.serius.infobook;
+
+public class MyChanges {
+    // hi
+}
